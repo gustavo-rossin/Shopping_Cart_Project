6586ffea5023f4ad786a46c2319c8283847c73b2
gustavo-rossin <gustavo.rossin@hotmail.com>
2022-09-16 10:19:39 -0300

    lint corrections on challenge 2

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -4,13 +4,11 @@ const fetchProducts = async (product) => {
     const url = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data.results);
     return data.results;
   } catch (error) {
     throw new Error('You must provide an url');
   }
 };
-fetchProducts('computador');
 if (typeof module !== 'undefined') {
   module.exports = {
     fetchProducts,
