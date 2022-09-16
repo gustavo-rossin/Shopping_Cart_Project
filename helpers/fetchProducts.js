46c605b2746bd73a84f7a61e4b600015f34cf15d
gustavo-rossin <gustavo.rossin@hotmail.com>
2022-09-16 10:08:21 -0300

    challenge 2 done

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -4,12 +4,13 @@ const fetchProducts = async (product) => {
     const url = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
     const response = await fetch(url);
     const data = await response.json();
-    return data;
+    console.log(data.results);
+    return data.results;
   } catch (error) {
     throw new Error('You must provide an url');
   }
 };
-
+fetchProducts('computador');
 if (typeof module !== 'undefined') {
   module.exports = {
     fetchProducts,
