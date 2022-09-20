831aa55cfe8c2d961393784c273daa13dafc2ec8
gustavo-rossin <gustavo.rossin@hotmail.com>
2022-09-20 12:46:39 -0300

    challenges  6 && 7 done

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -3,7 +3,14 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 
 localStorageSimulator('getItem');
 
-describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+describe('7 - Teste a função getSavedCartItems', () => {
+  it('7.1 - Teste se, ao executar getSavedCartItems, o método localStorage.getItem é chamado;', () => {
+    getSavedCartItems('cartItems')
+    expect(localStorage.getItem).toHaveBeenCalled();
+  })
+
+  it('7.2 - Teste se, ao executar getSavedCartItems, o método localStorage.getItem é chamado com o cartItems como parâmetro.', () => {
+    getSavedCartItems('cartItems')
+    expect(localStorage.getItem).toHaveBeenCalledWith('cartItems');
+  })
 });
--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -1,16 +1,19 @@
 const localStorageSimulator = require('../mocks/localStorageSimulator');
 const saveCartItems = require('../helpers/saveCartItems');
+const { expect } = require('@jest/globals');
 
 localStorageSimulator('setItem');
 
-describe('3 - Teste a função saveCartItems', () => {
+describe('6 - Teste a função saveCartItems', () => {
 
-  it('3.1 - Se o método localStorage.setItem é chamado na saveCartItems.', () => {
-    expect(saveCartItems()).toBe('cartItems');
+  it('6.1 - Se o método localStorage.setItem é chamado na saveCartItems.', () => {
+    saveCartItems('cartItems')
+    expect(localStorage.setItem).toHaveBeenCalled();
   })
 
-  it('3.2 - Se o método localStorage.setItem é chamado na saveCartItems, utilizando 2 parâmetros (o primeiro a chave cartItems e o segundo o valor passado para saveCartItems).', () => {
-    expect(saveCartItems('oi')).toBe('cartItems', 'oi');
+  it('6.2 - Se o método localStorage.setItem é chamado na saveCartItems, utilizando 2 parâmetros (o primeiro a chave cartItems e o segundo o valor passado para saveCartItems).', () => {
+    saveCartItems('olá')
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', 'olá');
   })
 
 });
